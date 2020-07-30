706bf8928775a9e4c253a57d959158ce5449bd91
Maximilian Wick <max@h2898373.stratoserver.net>
2020-07-30 07:33:44 +0200

    adjusts

---
+++ public/precache-manifest.09dbb06cfde242f25599efbc6645c4d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61852df9ce659a030e8cdd8e0814523c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f38f41235327ed4914d0",
+    "url": "/static/css/main.15e7e972.chunk.css"
+  },
+  {
+    "revision": "a34ee8f51f2467a9e43e",
+    "url": "/static/js/2.ba60c965.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ba60c965.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f38f41235327ed4914d0",
+    "url": "/static/js/main.e6d912f3.chunk.js"
+  },
+  {
+    "revision": "3b7b340aad85cb3b831f",
+    "url": "/static/js/runtime-main.536ac107.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
